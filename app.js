6e71a821a83f23d9aaa1461e09157d9aea66c73c
patlub <patricklu2010@gmail.com>
2018-03-13 22:30:55 +0300

    setup todos project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyPareser = require('body-parser');
+const logger = require('morgan');
+
+const app = express();
+
+// Log requests to the console
+app.use(logger('dev'));
+
+// Parse incoming requests data
+app.use(bodyPareser.json());
+app.use(bodyPareser.urlencoded({extended: false}));
+
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to cool stuff',
+}));
+
+module.exports = app;
