a3c28e3ebb379403e31e6f96d88967686bca8e46
patlub <patricklu2010@gmail.com>
2018-03-15 01:30:19 +0300

    add create, list, update for items

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,4 +1,5 @@
 const Todo = require('../models').Todo;
+const TodoItem = require('../models').TodoItem;
 
 module.exports = {
   create(req, res) {
@@ -11,8 +12,53 @@ module.exports = {
   },
   list(req, res) {
     return Todo
-      .all()
-      .then(todo => res.status(201).send(todo))
-      .catch(error => res.status(400).send(error));
+    .findAll({
+      include: [{
+        model: TodoItem,
+        as: 'todoItems',
+      }],
+    })
+    .then(todos => res.status(200).send(todos))
+    .catch(error => res.status(400).send(error));
+  },
+  retrieve(req, res) {
+    return Todo
+    .findById(req.params.todoId, {
+      include: [{
+        model: TodoItem,
+        as: 'todoItems',
+      }],
+    })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send({
+          message: 'Todo Not Found',
+        });
+      }
+      return res.status(200).send(todo);
+    })
+    .catch(error => res.status(400).send(error));
+  },
+  update(req, res) {
+    return Todo.findById(req.params.todoID, {
+      include: [{
+        model: TodoItem,
+        AS: 'todoItems',
+      }],
+    })
+      .then(todo => {
+        if (!todo) {
+          return res.status(400).send({
+            message: 'Todo not found',
+          });
+        }
+        return todo
+          .update({
+            title: req.body.title || todo.title,
+          })
+          .then(res.status(200).send(todo))
+          .catch((error) => res.status(400).send(error));
+      })
+        .catch((error) => res.status(400).send(error));
   },
 };
