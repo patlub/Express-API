fe1f06d613451650a52812196e6cc09f938347c5
patlub <patricklu2010@gmail.com>
2018-03-21 16:33:09 +0300

    implement user controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,2 +1,48 @@
 const User = require('../models').User;
-const Todo = require('../models').Todo;
+
+module.exports = {
+  create(req, res) {
+    return User
+      .create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+      })
+      .then(user => res.status(201).send(user))
+      .catch(error => res.status(400).send(error));
+  },
+  retrieve(req, res) {
+    return User
+      .findById(req.params.userId)
+      .then(user => {
+        if (!user) {
+          return res.status(404).send({
+            message: 'User not Found',
+          });
+        }
+        return res.status(200).send(user);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+  update(req, res) {
+    return User
+      .findById(req.params.userId)
+      .then(user => {
+        if (!user) {
+          return res.status(404).send({
+            message: 'user not fonund',
+          });
+        }
+        return user
+          .update({
+            firstName: req.body.firstname || user.firstname,
+            lastName: req.body.firstname || user.lastname,
+            email: req.body.firstname || user.email,
+            password: req.body.firstname || user.password,
+          })
+          .then(res.status(201).send(user))
+          .catch(error => res.status(400).send(error));
+      });
+  },
+};
